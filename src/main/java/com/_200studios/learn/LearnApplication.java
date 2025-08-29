2e1ebfa5628583c8dc79957d9e3feeab7b62e970
carlos-talavera <carloseduardorivera12@gmail.com>
2025-08-29 14:33:23 -0600

    chore: root

---
+++ src/main/java/com/_200studios/learn/LearnApplication.java
@@ -0,0 +1,13 @@
+package com._200studios.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(LearnApplication.class, args);
+	}
+
+}
