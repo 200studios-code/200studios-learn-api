c7524aab5633eed345c269832aa73c48212a7ad8
carlos-talavera <carloseduardorivera12@gmail.com>
2025-08-31 13:07:01 -0600

    feat(allowed-users): add getter annotation to allowed user dao

--- src/main/java/com/_200studios/learn/api/allowed_users/AllowedUser.java
+++ src/main/java/com/_200studios/learn/api/allowed_users/AllowedUser.java
@@ -1,11 +1,14 @@
 package com._200studios.learn.api.allowed_users;
 
 import com._200studios.learn.shared.types.GlobalRole;
+import com.fasterxml.jackson.annotation.JsonGetter;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.Instant;
 
 @Entity
+@Getter
 @Table(name = "allowed_users")
 public class AllowedUser {
     @Id
