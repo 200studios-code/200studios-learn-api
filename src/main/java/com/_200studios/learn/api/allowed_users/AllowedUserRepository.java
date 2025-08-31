fded549a28dd6e3db76b06c767d6e602c553bae3
carlos-talavera <carloseduardorivera12@gmail.com>
2025-08-31 12:09:38 -0600

    feat(allowed-users): add repository interface

---
+++ src/main/java/com/_200studios/learn/api/allowed_users/AllowedUserRepository.java
@@ -0,0 +1,5 @@
+package com._200studios.learn.api.allowed_users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AllowedUserRepository extends JpaRepository<AllowedUser, Long> {}
