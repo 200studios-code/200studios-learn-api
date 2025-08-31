b046dc38288a7434a123d6f6bb6a982c3860ab89
carlos-talavera <carloseduardorivera12@gmail.com>
2025-08-31 13:07:18 -0600

    feat(allowed-users): add method to find by email in repository

--- src/main/java/com/_200studios/learn/api/allowed_users/AllowedUserRepository.java
+++ src/main/java/com/_200studios/learn/api/allowed_users/AllowedUserRepository.java
@@ -2,4 +2,8 @@ package com._200studios.learn.api.allowed_users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AllowedUserRepository extends JpaRepository<AllowedUser, Long> {}
+import java.util.Optional;
+
+public interface AllowedUserRepository extends JpaRepository<AllowedUser, Long> {
+    Optional<AllowedUser> findByEmail(String email);
+}
