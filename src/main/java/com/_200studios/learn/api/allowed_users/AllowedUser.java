98a0f34876f36b2dd3078b1043b2ce3dbfc4bbcf
carlos-talavera <carloseduardorivera12@gmail.com>
2025-08-31 13:39:50 -0600

    refactor(allowed-users): simplify dao by using data annotation

--- src/main/java/com/_200studios/learn/api/allowed_users/AllowedUser.java
+++ src/main/java/com/_200studios/learn/api/allowed_users/AllowedUser.java
@@ -3,14 +3,14 @@ package com._200studios.learn.api.allowed_users;
 import com._200studios.learn.shared.types.GlobalRole;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Entity
-@Getter
-@Setter
+@Data
 @Table(name = "allowed_users")
 public class AllowedUser {
     @Id
