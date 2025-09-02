7638fe36ec9f974ddd2dc5665e9496d01fe57058
carlos-talavera <carloseduardorivera12@gmail.com>
2025-09-02 14:47:42 -0600

    feat(config): add environment enum

---
+++ src/shared/config/models/environment.enum.ts
@@ -0,0 +1,9 @@
+enum Environment {
+  LOCAL = 'local',
+  DEVELOPMENT = 'development',
+  STAGING = 'staging',
+  PRODUCTION = 'production',
+}
+
+export const { LOCAL, DEVELOPMENT, STAGING, PRODUCTION } = Environment;
+export default Environment;
