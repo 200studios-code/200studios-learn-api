117b5e67d709ba5021f2524c9cb28549d66bdd7d
carlos-talavera <carloseduardorivera12@gmail.com>
2025-09-02 14:48:06 -0600

    feat(config): add initial validation schema for environment variables

---
+++ src/shared/config/models/env-variables.model.ts
@@ -0,0 +1,22 @@
+import Environment from '@/shared/config/models/environment.enum';
+import { z } from 'zod';
+
+export const ENV_VARIABLES_SCHEMA = z.object({
+  NODE_ENV: z
+    .enum(Object.values(Environment) as [Environment, ...Environment[]])
+    .default(Environment.LOCAL),
+  PORT: z
+    .string()
+    .regex(/\d{4,5}/)
+    .default('5000'),
+  DATABASE_HOST: z.string().nonempty(),
+  DATABASE_NAME: z.string().nonempty(),
+  DATABASE_USERNAME: z.string().nonempty(),
+  DATABASE_PASSWORD: z.string().nonempty(),
+  DATABASE_PORT: z
+    .string()
+    .regex(/\d{4,5}/)
+    .nonempty(),
+});
+
+export type EnvVariables = z.infer<typeof ENV_VARIABLES_SCHEMA>;
