009ec13fc6c5fafbd331ddf61c8c889b63e1b495
carlos-talavera <carloseduardorivera12@gmail.com>
2025-09-03 10:04:16 -0600

    feat(allowed-users): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { AllowedUserModule } from '@/api/allowed-users/allowed-user.module';
 import { validate } from '@/shared/config/env.validation';
 import { PrismaModule } from '@/shared/datasource/prisma/prisma.module';
 import { Module } from '@nestjs/common';
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       validate,
     }),
     PrismaModule,
+    AllowedUserModule,
   ],
   controllers: [],
   providers: [],
