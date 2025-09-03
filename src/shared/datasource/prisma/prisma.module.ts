952972bd2a3eb86db6761e353e8572f0b1ac635d
carlos-talavera <carloseduardorivera12@gmail.com>
2025-09-03 09:37:51 -0600

    fix(db): include global prisma module
    This way there's no need to inject the prisma service in every module.

---
+++ src/shared/datasource/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { PrismaService } from '@/shared/datasource/prisma/prisma.service';
+import { Global, Module } from '@nestjs/common';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
