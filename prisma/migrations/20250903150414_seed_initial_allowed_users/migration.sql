88775134ccba6cd6fb424f73760c7507b30df420
carlos-talavera <carloseduardorivera12@gmail.com>
2025-09-03 09:51:57 -0600

    feat(db): add migration to seed initial allowed users

---
+++ prisma/migrations/20250903150414_seed_initial_allowed_users/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO allowed_users (email, global_role) VALUES ('200studios.info@gmail.com', 'ADMIN');
+INSERT INTO allowed_users (email, global_role) VALUES ('carloseduardorivera12@gmail.com', 'ADMIN');
